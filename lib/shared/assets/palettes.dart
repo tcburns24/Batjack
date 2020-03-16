e3ebfbc0ca685d03f6e1022274b0b5ce9fe68f0b
Tom Burns <tom@themighty.com>
2020-03-15 22:51:42 -0400

    Adds CasinoColor theme & styles drawer

---
+++ lib/shared/assets/palettes.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CasinoColors {
+  static const Color black = Color(0xff191210);
+  static const Color brown = Color(0xff50433B);
+  static const Color red = Color(0xffC41826);
+  static const Color pink = Color(0xffC9A1A8);
+  static const Color grey = Color(0xff887994);
+  static const Color purple = Color(0xff31092e);
+  static const Color gold = Color(0xffaf8439);
+  static const Color burnt = Color(0xff7e4029);
+  static const Color green = Color(0xff35654d);
+}
--- lib/shared/drawer.dart
+++ lib/shared/drawer.dart
@@ -1,5 +1,6 @@
 import 'package:blacktom/models/user.dart';
 import 'package:blacktom/services/database.dart';
+import 'package:blacktom/shared/assets/palettes.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -8,6 +9,7 @@ import 'loading.dart';
 class MainDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    double statusBar = MediaQuery.of(context).padding.top;
     final user = Provider.of<User>(context);
     return StreamBuilder<UserData>(
         stream: DatabaseService(uid: user.uid).gamblerData,
@@ -17,8 +19,34 @@ class MainDrawer extends StatelessWidget {
             return Drawer(
                 child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
+                Container(
+                    padding: EdgeInsets.symmetric(vertical: statusBar * 2, horizontal: 15),
+                    color: CasinoColors.purple,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Container(
+                            padding: EdgeInsets.only(top: 4, bottom: 4),
+                            child: Text(userData.username,
+                                style: TextStyle(
+                                    fontSize: 16, fontWeight: FontWeight.w600, fontFamily: 'Avenir', color: Colors.white))),
+                        Container(
+                            padding: EdgeInsets.only(top: 4, bottom: 4),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: <Widget>[
+                                Icon(
+                                  Icons.monetization_on,
+                                  color: Colors.white,
+                                ),
+                                Text(' Chips: ${userData.chips}  |   Level: ${userData.level}',
+                                    style: TextStyle(fontSize: 14, fontFamily: 'Avenir', color: Colors.white)),
+                              ],
+                            ))
+                      ],
+                    )),
                 ListTile(title: Text('Leaderboard'), leading: Icon(Icons.casino)),
                 ListTile(title: Text('Sign Out'), leading: Icon(Icons.flight_takeoff)),
               ],
